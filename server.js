09f0e163ff4715d030b0c6d1a3c014a6dde8e304
jmsoleto <jmsoleto@gmail.com>
2015-08-16 10:02:54 +0200

    Seteo de las partes del servidor y prueba inicial.

---
+++ server.js
@@ -0,0 +1,36 @@
+// CONFIGRUACION INICIAL
+
+//Importacion de paquetes
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+//Hacemos que express use body-parser
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//Seteamos el puerto de entrada de la app
+
+var port = process.env.PORT || 8080;
+
+// ENRUTADOR
+
+var router = express.Router();
+
+// Esto setea una prueba para comprobar que todo esta instalado correctamente. Se puede acceder a ella
+// a traves de http://localhost:8080/api
+
+router.get('/', function(req,res){
+  res.json({message:'Si puedes leer esto es que todo esta bien montado'})
+})
+
+//REGISTRO DE RUTAS
+//todas las rutas de la api estaran en la ruta /api
+
+app.use('/api',router);
+
+//INICIALIZACION DEL SERVIDOR
+
+app.listen(port);
