a06850e1900219c45d097af9ca9b3fea9e22ece2
Jose Manuel Soleto <jmsoleto@gmail.com>
2015-08-17 12:12:05 +0200

    Added delete route and format code

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var Bear = require('./app/models/Bear');
 
 //Hacemos que express use body-parser
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 //Seteamos el puerto de entrada de la app
@@ -26,16 +28,18 @@ mongoose.connect('localhost:27017/bears')
 var router = express.Router();
 
 //Cada vez que recibamos cualquier petición mostramos un log
-router.use(function(req,res,next){
-  console.log("Se ha recibido una petición");
-  next();
+router.use(function(req, res, next) {
+    console.log("Se ha recibido una petición");
+    next();
 })
 
 // Esto setea una prueba para comprobar que todo esta instalado correctamente. Se puede acceder a ella
 // a traves de http://localhost:8080/api
 
-router.get('/', function(req,res){
-  res.json({message:'Si puedes leer esto es que todo esta bien montado'})
+router.get('/', function(req, res) {
+    res.json({
+        message: 'Si puedes leer esto es que todo esta bien montado'
+    })
 })
 
 
@@ -43,53 +47,69 @@ router.get('/', function(req,res){
 //REGISTRO DE RUTAS
 //todas las rutas de la api estaran en la ruta /api
 
-app.use('/api',router);
+app.use('/api', router);
 
 router.route('/bears')
-      .post(function(req,res){
+    .post(function(req, res) {
         var bear = new Bear();
         bear.name = req.body.name;
-        bear.save(function(err){
-          if(err){
-            res.send(err);
-          }
-          res.json({message:'Oso creado'})
+        bear.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({
+                message: 'Oso creado'
+            })
         })
-      })
-      .get(function(req,res){
-        Bear.find(function(err,bears){
-          if(err)
-            res.send(err)
+    })
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err)
+                res.send(err)
 
-          res.json(bears);
+            res.json(bears);
         })
-      })
+    })
 
 
 router.route('/bears/:bears_id')
-  .get(function(req,res){
-    Bear.findById(req.params.bear_id, function(err,bear){
-      if(err)
-        res.send(err);
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
 
-      res.json(bear)
+            res.json(bear)
+        })
     })
-  })
-  .put(function(req,res){
-    Bear.findById(req.params.bear_id, function(err,bear){
-      console.log(bear);
-      if(err)
-          res.send(err)
+    .put(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            console.log(bear);
+            if (err)
+                res.send(err)
 
-      bear.name = req.body.name;
-      bear.save(function(err){
-        if(err)
-          res.send(err)
+            bear.name = req.body.name;
+            bear.save(function(err) {
+                if (err)
+                    res.send(err)
 
-        res.json({message:"Oso actualizado"})
-      })
+                res.json({
+                    message: "Oso actualizado"
+                })
+            })
+        })
     })
-  })
+    .delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err) {
+            if (err)
+                res.send(err)
+
+            res.json({
+                message: "Oso borrado"
+            })
+        })
+    })
 
 //INICIALIZACION DEL SERVIDOR
 
